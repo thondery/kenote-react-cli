1e44567ebb70d4a3245784d9c89b4f0ebb251df5
thondery <thondery@163.com>
2017-11-07 16:53:11 +0800

    add scaffold...

--- src/index.js
+++ src/index.js
@@ -5,5 +5,7 @@ exports.config = require('./config')
 exports.scaffold = require('./scaffold')
 
 exports.scaffoldOptions = {
-  ['admin']: 'https://github.com/thondery/kenote-react-admin.git'
+  ['admin']: 'https://github.com/thondery/kenote-react-admin.git',
+  ['base']: 'https://github.com/thondery/kenode-react-base.git',
+  ['component']: 'https://github.com/thondery/kenote-react-component.git'
 }
